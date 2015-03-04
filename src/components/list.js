31685beb21e0a29cfb488e15e1f24e8cc660b27d
KungD <david@krawaller.se>
2015-03-04 18:46:24 +0100

    bugfix

--- src/components/list.js
+++ src/components/list.js
@@ -17,7 +17,7 @@ var List = React.createClass({
     },
     render: function() {
         var outof = this.props.outof, by = this.state.by, mod = this.state.down ? 1 : -1, list = this.props.units.sort(function(u1,u2){
-            return (u1[by]||0 > u2[by]||0 ? -1 : 1)*mod;
+            return ((u1[by]||0) > (u2[by]||0) ? -1 : 1)*mod;
         });
         var n=0, printTH = function(prop){
             return <th key={n++} onClick={this.sortBy.bind(this,prop)}>{prop[0].toUpperCase()+prop.substr(1,666)}{prop===by?(this.state.down?'↑':'↓'):' '}</th>
@@ -47,7 +47,7 @@ These are the {list.length} matched units ({Math.round(100*list.length/outof)}% 
                         </tr>
                     </thead>
                     <tbody>
-                		{this.props.units.map(function(unit){
+                		{list.map(function(unit){
                 			return <Unit key={unit.id} unit={unit}/>;
                 		})}
                     </tbody>
