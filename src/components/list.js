eba5b90c99e5902fca0283b1bd249c52ddfde99d
KungD <david@krawaller.se>
2015-03-04 07:00:23 +0100

    fix

--- src/components/list.js
+++ src/components/list.js
@@ -17,7 +17,7 @@ var List = React.createClass({
     },
     render: function() {
         var outof = this.props.outof, by = this.state.by, mod = this.state.down ? 1 : -1, list = this.props.units.sort(function(u1,u2){
-            return (u1[by] > u2[by] ? -1 : 1)*mod;
+            return (u1[by]||0 > u2[by]||0 ? -1 : 1)*mod;
         });
         var n=0, printTH = function(prop){
             return <th key={n++} onClick={this.sortBy.bind(this,prop)}>{prop[0].toUpperCase()+prop.substr(1,666)}{prop===by?(this.state.down?'↑':'↓'):' '}</th>
